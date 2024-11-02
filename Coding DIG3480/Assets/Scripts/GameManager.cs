ffd4155228ea048a078c18d672ce53f06f8bae00
Sinnist3r <ryanisryanbutryan@gmail.com>
2024-11-02 02:39:42 -0400

    Brian Huang - TASK 2
    Added "EnemyNew" to the game.
    
    - Prefab "EnemyNew" Created
    - Script "EnemyNew.cs" Created
    - Adjusted "GameManager.cs" to accomodate "EnemyNew"

--- Coding DIG3480/Assets/Scripts/GameManager.cs
+++ Coding DIG3480/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemyNew;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,7 @@ public class GameManager : MonoBehaviour
         //Create and set the players Y to -3 and repeat void CreateEnemy
         Instantiate(player, transform.position = new Vector3(0, -3, 0), Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemyNew", 1f, 5f);
     }
 
     // Update is called once per frame
@@ -27,5 +29,10 @@ public class GameManager : MonoBehaviour
     {
         //Create enemy at the top of the screen
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyNew()
+    {
+        Instantiate(enemyNew, new Vector3(Random.Range(-9f, 9f), -12f, 0), Quaternion.identity);
     }
 }
